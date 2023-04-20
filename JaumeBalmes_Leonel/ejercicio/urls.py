959fa104d46e1b8eca25404ae7dffe3929c0ce49
leonel-ruiz-villacis <lruiz19@jaumebalmes.net>
2023-04-20 20:32:18 +0200

    acabat

--- JaumeBalmes_Leonel/ejercicio/urls.py
+++ JaumeBalmes_Leonel/ejercicio/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('student/<int:student_id>/', views.student_detail, name='student_detail'),
     path('teacher/<int:teacher_id>/', views.teacher_detail, name='teacher_detail'),
     path('formulario/', views.formulario, name='formulario'),
+    path('add_student/', views.add_student, name='add_student'),
+    path('update_student/<int:student_id>/', views.update_student, name='update_student'),
+    path('delete_student/<int:student_id>/', views.delete_student, name='delete_student'),
+    path('add_teacher/', views.add_teacher, name='add_teacher'),
+    path('update_teacher/<int:teacher_id>/', views.update_teacher, name='update_teacher'),
+    path('delete_teacher/<int:teacher_id>/', views.delete_teacher, name='delete_teacher'),
 ]
 
 
